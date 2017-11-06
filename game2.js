76b04c28b15751a4b16694f797b716ae7b2a867f
Andrew Hatfield <andrew8412@gmail.com>
2017-11-05 22:13:34 -0500

    added input test

--- game2.js
+++ game2.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 
 const wordlist = [
     "dog",
@@ -8,47 +8,74 @@ const wordlist = [
     "lion"
 ]
 
+// ====================================================================================
+
 // START THE GAME
 Game();
 
 // GAME FUNCTION
 function Game() {
+
+    startNewGame();
+
     function startNewGame() {
-        console.log("Guess the word!");
+        console.log("\nGuess the word!");
         this.guessesLeft = 10;
         nextWord();
+
     }
+
     function nextWord() {
         var randomWord = wordlist[Math.floor(Math.random() * wordlist.length)];
-        console.log('randomWord === ' + randomWord);
+        var randomWordTwo = "second argument";
+        // console.log('randomWord === ' + randomWord);
         this.currentWord = new Word(randomWord);
         // console.log('currentWord === ' + this.currentWord);
-    }
-    startNewGame();
-}
-// ================================
 
-function Word(word) {
-    this.letters = word.split('').map(function(char) {
-        return new Letter(char);
+        // ========= INQUIRER ===============
+        //
+        // get INPUT and check if INPUT === currentWord.letters[].item
+        //
+        //
+        // this.solve = function(x) {
+        //     if(x === "c") {
+        //         console.log("foo");
+        //     } else {
+        //         console.log("bar")
+        //     }
+        // }
+        // console.log(solve(this.currentWord.letters[0].item));
+    }
+}
+// ====================================================================================
+
+
+function Word(arg1) {
+    this.letters = arg1.split('').map(function(item) {
+        return new Letter(item);
     });
+    // console.log(this.letters);    
+
     this.displayLetters = function() {
         return this.letters.map(function(letter) {
-            return letter.hideLetter();
-        })
+            return letter.displayLetter();
+        }).join(" ");
     }
     console.log(this.displayLetters());
-    // console.log(this.letters[0].char);
+    // console.log(this.letters[0].item);
 }
 
-function Letter(char) {
-    this.char = char;
+// ====================================================================================
+
+function Letter(item) {
+    this.item = item;
     this.discovered = false;
-    this.hideLetter = function() {
+    this.displayLetter = function() {
         if(this.discovered === false) {
             return "_";
-        } else {
-            return this.char;
+        }
+        else if(this.discovered === true) {
+            return this.item;
         }
     }
 }
--- test.js
+++ test.js
@@ -1,13 +1,27 @@
-function test(arg1, arg2) {
-    var r;
-    r = arg1 * arg2;
-    return (r)
-}
+// function test(arg1, arg2) {
+//     var r;
+//     r = arg1 * arg2;
+//     return (r)
+// }
 
-// test(2,3);
-// console.log(test(2,3));      // works
+// // test(2,3);
+// // console.log(test(2,3));      // works
 
 
-var numbers = [2, 4, 8, 10];
-var halves = numbers.map(x => x / 2);
-console.log(halves);
+// var numbers = [2, 4, 8, 10];
+// // var halves = numbers.map(x => x / 2);
+// var halves = numbers.map(function(x) {
+//     return (x / 2);
+// })
+// console.log(halves);
+
+var lettersGuessed = ["dog","cat","hello"];
+
+function replay() {
+    lettersGuessed = [];
+    console.log(lettersGuessed);
+} 
+
+console.log(lettersGuessed)
+
+replay();
