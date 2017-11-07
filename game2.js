8ae0a252a20ed2f9a4cd807ea0da25d290c1dca7
Andrew Hatfield <andrew8412@gmail.com>
2017-11-06 19:44:42 -0500

    added input and letter discovery. Still need to add inquirer

--- game2.js
+++ game2.js
@@ -1,4 +1,4 @@
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 
 const wordlist = [
     "dog",
@@ -17,59 +17,92 @@ Game();
 function Game() {
     
     console.log("\nGuess the word!");
-    this.guessesLeft = 10;
+    var guessesLeft = 10;
+    function subtractGuessesLeft() {
+        guessesLeft -= 1;
+        return guessesLeft ;
+    }
     var randomWord = wordlist[Math.floor(Math.random() * wordlist.length)];
-    // console.log('randomWord === ' + randomWord);
+    console.log('randomWord === ' + randomWord);
     // console.log(typeof(randomWord));
     this.currentWord = new Word(randomWord);
     // console.log('this.currentWord === ' + this.currentWord);
     // console.log('typeof(this.currentWord) === ' + typeof(this.currentWord));
 
-
-    // ========= INQUIRER ===============
-    //
-    // get INPUT and check if INPUT === currentWord.letters[].item
-    //
-    //
-    // this.solve = function(x) {
-    //     if(x === "c") {
-    //         console.log("foo");
-    //     } else {
-    //         console.log("bar")
-    //     }
-    // }
-    // console.log(solve(this.currentWord.letters[0].item));
+    var input = process.argv[2];
+    // console.log(this.currentWord.letters);                                           // Array to loop through
+    // console.log(this.currentWord.letters.map(item => typeof(item.character)));       // Checks the typeof of each item.character
+    // console.log(this.currentWord.letters[0].displayLetter());                        // function to invoke for each
+    function displayWord() {
+        var word = this.currentWord.letters.map(
+            function(item) {
+                if(item.character === input) {
+                    console.log("You discovered a letter!")
+                    item.discovered = true;
+                    return item.displayLetter();
+                } else {
+                    return item.displayLetter();
+                }
+            }
+        ).join(" ");
+        console.log(word);       
+    }
+    displayWord();
 }
 
 // ====================================================================================
 
 
 function Word(arg1) {
-    this.letters = arg1.split('').map(function(item) {
-        return new Letter(item);
-    });
-    // console.log(this.letters);    
-
-    this.displayLetters = function() {
-        return this.letters.map(function(letter) {
-            return letter.displayLetter();
-        }).join(" ");
-    }
-    console.log(this.displayLetters());
-    // console.log(this.letters[0].item);
+    this.letters = arg1.split('').map(
+        function(arrayItem) {
+            return new Letter(arrayItem);
+        }
+    );
+    // console.log("this.letters === " + this.letters);    
+    // console.log("typeofthis.letters === " + typeof(this.letters));
 }
 
 // ====================================================================================
 
-function Letter(item) {
-    this.item = item;
+function Letter(character) {
+    this.character = character;
     this.discovered = false;
     this.displayLetter = function() {
         if(this.discovered === false) {
             return "_";
         }
         else if(this.discovered === true) {
-            return this.item;
+            return this.character;
         }
     }
 }
+
+
+
+
+// var randomWord = "dog";
+// var currentWord = new Word(randomWord);
+
+// word(randomWord) {
+//     // 1. "dog" Take the argument 
+//     // 2. ["d","o","g"] Turn dog into an array with .split("")
+//     // 3. [{},{},{}] Transforms each item of the array into an object using .map()
+//     [ 
+//         { 
+//             this.character = "d", 
+//             this.discovered = false, 
+//             this.displayLetter = [function] 
+//         } , 
+//         {
+//             this.character = "o",
+//             this.discovered = false,
+//             this.displayLetter = [function]
+//         }, 
+//         {
+//             this.character = "g",
+//             this.discovered = false,
+//             this.displayLetter = [function]
+//         } 
+//     ]
+// }
