ef2417b802d5aa80969fddf68ca752b695d5821f
Andrew Hatfield <andrew8412@gmail.com>
2017-11-11 20:24:16 -0500

    added introduction to building the hangman game

---
+++ intro_game.js
@@ -0,0 +1,32 @@
+// create a new word where each letter of that word will:
+    // 1) display itself when discovered 
+    // OR 
+    // 2) display _ when not discovered
+
+// word = dog   // display _ _ _
+// input = 'd'  // display d _ _
+// input = 'o'  // display d o _
+
+// .split('')   // transforms string into array
+// .join(' ')   // transforms array into string
+// .map()       // transforms each item of an array to something else
+// =============================================================================================
+
+
+var currentWord = new Word("dog");
+
+// console.log(currentWord);                       // Word { string: 'dog', stringArray: ['d', 'o', 'g'], objectArray = [object object] }
+// console.log(currentWord.string);                // 'dog'
+// console.log(currentWord.stringArray);           // ['d', 'o', 'g']
+// console.log(currentWord.objectArray);           // [ Letter{ charater: 'd' }, Letter{ character: 'o' }, Letter{ character: 'g' } ]
+
+function Word(str) {
+    this.string = str;                                                  // "dog"
+    this.stringArray = str.split('');                                   // ['d', 'o', 'g']
+    this.objectArray = this.stringArray.map(item => new Letter(item));  // [ Letter{ charater: 'd' }, Letter{ character: 'o' }, Letter{ character: 'g' } ]
+}
+
+function Letter(str) {
+    this.character = str;
+}
+
